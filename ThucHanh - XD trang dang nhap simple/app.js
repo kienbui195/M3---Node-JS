c2c56448bad230c4fe224844c2cfacf7c9a6304a
Kay Bi <kaybi9x@gmail.com>
2022-08-09 11:30:03 +0700

    update

---
+++ ThucHanh - XD trang dang nhap simple/app.js
@@ -0,0 +1,10 @@
+http = require('http')
+http.createServer((req,res) => {
+    let txt = ''
+    if (req.url === "/login") {
+        txt = 'Login Success!'
+    } else {
+        txt = 'Login Fail'
+    }
+    res.end(txt)
+}).listen(8080, 'localhost')
